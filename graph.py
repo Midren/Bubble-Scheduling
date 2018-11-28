8112e668827a8b41522b7d24c09ce4b3bd42c7e7
Momka45 <milromchuk@gmail.com>
2018-11-28 03:40:07 +0200

    Preparing for CPN-First Ordering

--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 class Graph:
     def __init__(self):
+        # dict with indicidence lists
         self.inc_dct = defaultdict(list)
 
     def __repr__(self):
@@ -13,6 +14,14 @@ class Graph:
 
     def children(self, node):
         """Returns children of the given vertice`s"""
+
+    def get_parents(self, node):
+        # Test
+        parents = []
+        for nd in self.get_nodes():
+            if node in self.get_children(nd):
+                parents.append(nd)
+        return parents
 
 
     def dijkstra(self, start):
@@ -40,7 +49,53 @@ class Graph:
                     path[transfer.children[-1]] = min_n
         return visited, path
 
+    def find_cpn_list(self):
+        # Test
+        fst, lst = None, None
+        for i in self.get_nodes():
+            if i.index == 0:
+                fst = i
+            elif len(self.inc_dct[i]) == 0:
+                lst = i
+            if fst is not None and lst is not None:
+                break
+        # This don't work now -- Yura fix pls
+        cp = self.dijkstra(self, fst)[lst]
+        return cp
 
+    def find_ibn_list(self):
+        # Test
+        cpn = self.find_cpn_list()
+        ibn = []
+        for node in self.get_nodes():
+            children = self.get_children(node)
+            for nd in cpn:
+                if nd in children:
+                    ibn.append(node)
+            else:
+                continue
+            break
+        return ibn
+
+    def topological_sort(self):
+        # Test
+        visited = defaultdict(bool)
+        nodes = []
+
+        def topological_sort_util(self, v, visited, nodes):
+            visited[v] = True
+
+            for i in self.get_children(v):
+                if visited[i] == False:
+                    topological_sort_util(self, i, visited, nodes)
+
+            nodes.insert(0, v)
+
+        for i in self.get_nodes():
+            if visited[i] == False:
+                topological_sort_util(self, i, visited, nodes)
+
+        return nodes
 
 
 class Node:
--- task_graph_generator.py
+++ task_graph_generator.py
@@ -1,33 +1,36 @@
 from graph import Node, Graph
 
-with open("dag.txt") as f:
+def get_graph(filename):
     graph = Graph()
-    lines = f.readlines()
-    end = int((lines[-1]).split()[1])
-    node_lst = [None for i in range(end+1)]
-    for line in lines[:-1]:
-        if(not line.startswith("NODE ")):
-            continue
-        line = line.split()
-        if line[3] == 'ROOT' or line[3] == 'COMPUTATION':
-            node = Node(int(line[1]), float(line[4]))
-            node_lst[node.index] = node
-            if line[2] == str(end):
-                graph.inc_dct[node].append((end, 0))
-            if line[3] == 'ROOT':
+    with open(filename) as f:
+        lines = f.readlines()
+        end = int((lines[-1]).split()[1])
+        node_lst = [None for i in range(end+1)]
+        for line in lines[:-1]:
+            if(not line.startswith("NODE ")):
+                continue
+            line = line.split()
+            if line[3] == 'ROOT' or line[3] == 'COMPUTATION':
+                node = Node(int(line[1]), float(line[4]))
+                node_lst[node.index] = node
+                if line[2] == str(end):
+                    graph.inc_dct[node].append((end, 0))
+                if line[3] == 'ROOT':
+                    for ind in line[2].split(","):
+                        graph.inc_dct[node].append((int(ind), 0))
+            else:
+                weight = float(line[4])
+                node_lst[node.index] = node
                 for ind in line[2].split(","):
-                    graph.inc_dct[node].append((int(ind), 0))
-        else:
-            weight = float(line[4])
-            node_lst[node.index] = node
-            for ind in line[2].split(","):
-                graph.inc_dct[node].append((int(ind), weight))
-    node_lst[end] = Node(end, 0)
-    for nd in graph.inc_dct.keys():
-        for ind in graph.inc_dct[nd][:]:
-            if isinstance(ind[0], int):
-                graph.inc_dct[nd].append((node_lst[ind[0]], ind[1]))
-                graph.inc_dct[nd].remove(ind)
-    graph.inc_dct[node_lst[-1]] = []
+                    graph.inc_dct[node].append((int(ind), weight))
+        node_lst[end] = Node(end, 0)
+        for nd in graph.inc_dct.keys():
+            for ind in graph.inc_dct[nd][:]:
+                if isinstance(ind[0], int):
+                    graph.inc_dct[nd].append((node_lst[ind[0]], ind[1]))
+                    graph.inc_dct[nd].remove(ind)
+        graph.inc_dct[node_lst[-1]] = []
+    return graph
 
-    print(graph)
+graph = get_graph("dag.txt")
+print(graph.topological_sort())
