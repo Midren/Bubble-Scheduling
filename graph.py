9b2ed1d83c85261bf370e05e31d183e864a12feb
Momka45 <milromchuk@gmail.com>
2018-11-28 02:23:14 +0200

    Wrote task graph generator

--- graph.py
+++ graph.py
@@ -1,5 +1,16 @@
+from collections import defaultdict
+
 class Node:
     def __init__(self, ind, comp):
         self.index = ind
         self.computation = comp
-        
+
+    def __repr__(self):
+        return "Node( " + str(self.index) +", " + str(self.computation) + " )"
+
+class Graph:
+    def __init__(self):
+        self.inc_dct = defaultdict(list)
+
+    def __repr__(self):
+        return repr(self.inc_dct.items())
--- task_graph_generator.py
+++ task_graph_generator.py
@@ -1,6 +1,32 @@
+from graph import Node, Graph
+
 with open("dag.txt") as f:
-    graph = dict()
-    for line in f.readlines():
+    graph = Graph()
+    lines = f.readlines()
+    end = int((lines[-1]).split()[1])
+    node_lst = [None for i in range(end+1)]
+    for line in lines[:-1]:
         if(not line.startswith("NODE ")):
             continue
-        print(line.split())
+        line = line.split()
+        if line[3] == 'ROOT' or line[3] == 'COMPUTATION':
+            node = Node(int(line[1]), float(line[4]))
+            node_lst[node.index] = node
+            if line[2] == str(end):
+                graph.inc_dct[node].append((end, 0))
+            if line[3] == 'ROOT':
+                for ind in line[2].split(","):
+                    graph.inc_dct[node].append((int(ind), 0))
+        else:
+            weight = float(line[4])
+            node_lst[node.index] = node
+            for ind in line[2].split(","):
+                graph.inc_dct[node].append((int(ind), weight))
+    node_lst[end] = Node(end, 0)
+    for nd in graph.inc_dct.keys():
+        for ind in graph.inc_dct[nd][:]:
+            if isinstance(ind[0], int):
+                graph.inc_dct[nd].append((node_lst[ind[0]], ind[1]))
+                graph.inc_dct[nd].remove(ind)
+
+    print(graph)
