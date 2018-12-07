fe8b82602669d61cb57fd8511fdfb4cf4ddb7123
Momka45 <milromchuk@gmail.com>
2018-12-07 20:35:44 +0200

    Preparation continues

---
+++ processor_graph.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+class ProcessorsGraph:
+    def __init__(self):
+        self.inc_dct = defaultdict(lst)
+
+class Processor:
+    def __init__(self):
+        self.tasks = []
